e4ba84cc425664a5c9d5d8ce49b8d01013128a60
Kevin Nam <dong.g.nam@mail.mcgill.ca>
2021-12-20 23:58:10 -0500

    pushing to saggy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,3 @@
-import * as functions from "firebase-functions";
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +5,36 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// functions/index.js
+const functions = require("firebase-functions");
+const faker = require("faker");
+
+// Initialize products array
+const products: { name: any; price: any }[] = [];
+
+// Max number of products
+const LIMIT = 100;
+
+// Push a new product to the array
+for (let i = 0; i < LIMIT; i++) {
+  products.push({
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+  });
+}
+
+exports.listProducts = functions.https.onCall((data: any, context: any) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "Endpoint requires authentication!"
+    );
+  }
+  const { page = 1, limit = 10 } = data;
+
+  const startAt = (page - 1) * limit;
+  const endAt = startAt + limit;
+
+  return products.slice(startAt, endAt);
+});
